3ee7850ed82e3beb6d6a46c28b52c408f0a3132f
saikalyansennam <saikalyansennam66@gmail.com>
2025-03-27 22:19:48 +0530

    exploreSetand get method

---
+++ src/test/java/com/sai/Animal.java
@@ -0,0 +1,20 @@
+package com.sai.pojo;
+
+public class Animal {
+	private int age;
+	private String name;
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+
+}
