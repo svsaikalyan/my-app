f5dec3164ddb769a887b0c248951d02cb5123aa9
saikalyansennam <saikalyansennam66@gmail.com>
2025-03-27 22:21:40 +0530

    exploreExample

---
+++ src/test/java/com/sai/TestAnimal.java
@@ -0,0 +1,11 @@
+package com.sai.pojo;
+
+public class TestAnimal {
+	public static void main(String[] args) {
+		Animal ar= new Animal();
+		ar.setAge(24);
+//		ar.setName("komal");
+//	System.out.println("The age is "+ar.getAge()+" and name is "+ar.getName());
+	}
+
+}
