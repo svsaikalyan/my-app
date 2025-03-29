0f64acfda62f275d3ea1d4035bdff99c866c34fa
saikalyansennam <saikalyansennam66@gmail.com>
2025-03-29 20:05:24 +0530

    exploreExample

---
+++ src/test/java/com/sai/Gorilla.java
@@ -0,0 +1,13 @@
+package com.sai.pojo;
+
+public class Gorilla extends Aniimal{
+	public static void main(String[] args) {
+//		super(age, "Gorilla");
+		
+	}
+	public Gorilla() {
+		super(5);
+		
+	}
+
+}
