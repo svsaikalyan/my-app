87bb9956438b3414a1379f6916b42acda4eef69a
saikalyansennam <saikalyansennam66@gmail.com>
2025-03-30 06:43:03 +0530

    exploreExample

---
+++ src/test/java/com/sai/Turtle.java
@@ -0,0 +1,16 @@
+package com.sai.pojo;
+
+public class Turtle {
+	private String name;
+	public Turtle() {
+		name = "saikrishna";
+	}
+	public Turtle(int age) {
+	}
+	public Turtle(Long age) {
+	}
+	public Turtle(String newName, String...favoriteFoods) {
+		name  = newName;
+	}
+
+}
---
+++ src/test/java/com/sai/Zoo.java
@@ -0,0 +1,9 @@
+package com.sai.pojo;
+
+public class Zoo {
+	public Zoo () {
+		super();
+		System.out.println("Zoo created");
+	}
+
+}
